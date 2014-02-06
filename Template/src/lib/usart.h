43b9256e90209f7e8214aa065ead8b0f0b2bc471
grosi <grosi.mail@gmail.com>
2014-02-06 15:06:07 +0100

    change pin-configuration

--- Template/src/lib/usart.h
+++ Template/src/lib/usart.h
@@ -26,14 +26,14 @@ typedef void (*CANRxCatch_t)(CanRxMsg); /* function-handler to application-area 
 
 /* exported define -----------------------------------------------------------*/
 /* Pin and Port of the USART-Interface */
-#define USART_PIN_TX_NUMBER     6
-#define USART_PIN_RX_NUMBER     7
+#define USART_PIN_TX_NUMBER     8
+#define USART_PIN_RX_NUMBER     9
 #define USART_PINS_MODE         GPIO_Mode_AF
 #define USART_PINS_SPEED        GPIO_Speed_50MHz
 #define USART_PINS_TYPE         GPIO_OType_PP
 #define USART_PINS_PUPD         GPIO_PuPd_UP
-#define USART_PORT_LETTER       'B' /*!< port A to D */
-#define USART_INTERFACE_NUMBER  1 /*!< USART number (1 to 6) */
+#define USART_PORT_LETTER       'D' /*!< port A to D */
+#define USART_INTERFACE_NUMBER  3 /*!< USART number (1 to 6) */
 #define USART_BAUD              115200 /*!< kBit/s Baudrate */
 #define USART_WORDLENGTH        USART_WordLength_8b
 #define USART_STOPBITS          USART_StopBits_1
