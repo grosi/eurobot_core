476d18eed80578e25391a87b816ddad1be4440b3
grosi <grosi.mail@gmail.com>
2014-03-17 18:14:23 +0100

    -add the first few lines for node-scheduler -track enemy function implemented

--- Template/src/application/Strategy.c
+++ Template/src/application/Strategy.c
@@ -1,6 +1,6 @@
 /**
  * \file    Strategy.h
- * \author  meert1
+ * \author  gross10
  * \date    2013-11-29
  *
  * \version 1.0
@@ -20,6 +20,8 @@
 #include "app_config.h"
 #include "CANGatekeeper.h"
 #include "Strategy.h"
+#include "nodes/node_config.h"
+
 
 /* HW-library */
 //#include "..\lib\roboBoardInterface.h" /*!< \todo remove if not used */
@@ -34,10 +36,21 @@
 
 
 /* Private variables ---------------------------------------------------------*/
+static xTaskHandle xNodeTask1_Handle;
+static xTaskHandle xNodeTask2_Handle;
+static uint8_t state_node_1 = NODE_STOP;
+static uint8_t state_node_2 = NODE_STOP;
+static node_t* nodes[NODE_QUANTITY] = {&node_mammut_1};
+static node_t* node_task_1;
+static node_t* node_task_2;
+static float enemey_position[(int)(PLAYGROUND_HEIGH/ENEMY_GRID_SIZE_Y)][(int)(PLAYGROUND_WIDTH/ENEMY_GRID_SIZE_X)] = {0.0};
 
 
 /* Private function prototypes -----------------------------------------------*/
-static void vStrategyTask(void*);
+static void vStrategySchedulerTask(void*);
+static void vNodeTask1(void*);
+static void vNodeTask2(void*);
+static void vTrackEnemy(uint16_t, CAN_data_t*);
 
 /* Private functions ---------------------------------------------------------*/
 
@@ -52,19 +65,31 @@ static void vStrategyTask(void*);
 void initStrategyTask(void){
 
     /* create the task */
-    xTaskCreate( vStrategyTask, ( signed char * ) STRATEGY_TASK_NAME, STRATEGY_STACK_SIZE, NULL, STRATEGY_TASK_PRIORITY, NULL );
+    xTaskCreate( vStrategySchedulerTask, ( signed char * ) STRATEGY_SCHEDULER_TASK_NAME, STRATEGY_STACK_SIZE, NULL, STRATEGY_SCHEDULER_TASK_PRIORITY, NULL );
+    xTaskCreate( vNodeTask1, ( signed char * ) STRATEGY_NODE_TASK_1_NAME, STRATEGY_STACK_SIZE, NULL, STRATEGY_NODE_TASK_PRIORITY, xNodeTask1_Handle );
+    xTaskCreate( vNodeTask2, ( signed char * ) STRATEGY_NODE_TASK_2_NAME, STRATEGY_STACK_SIZE, NULL, STRATEGY_NODE_TASK_PRIORITY, xNodeTask2_Handle );
+
+    /* suspend the node task until they will used */
+    vTaskSuspend(xNodeTask1_Handle);
+    vTaskSuspend(xNodeTask2_Handle);
+
+    /* set CAN listeners */
+    setFunctionCANListener(vTrackEnemy,ENEMEY_1_POSITION_RESPONSE);
+    setFunctionCANListener(vTrackEnemy,ENEMEY_2_POSITION_RESPONSE);
 
 }
 
+
 /**
- * \fn          vStrategyTask
+ * \fn          vStrategySchedulerTask
  * \brief       Task to handle the strategy
  *
  * \param[in]   None
  * \return      None
  */
-static void vStrategyTask(void* pvParameters ) {
-
+static void vStrategySchedulerTask(void* pvParameters )
+{
+    /* local variables */
     portTickType xLastFlashTime;
 
     /* We need to initialise xLastFlashTime prior to the first call to
@@ -81,6 +106,87 @@ static void vStrategyTask(void* pvParameters ) {
 
     }
 }
+
+
+/**
+ * \fn          vNodeTask1
+ * \brief       Task to handle the strategy
+ *
+ * \param[in]   None
+ * \return      None
+ */
+static void vNodeTask1(void* pvParameters )
+{
+    /* endless */
+    for(;;)
+    {
+        node_task_1->node_function(&node_task_1->param,&state_node_1);
+    }
+}
+
+
+/**
+ * \fn          vNodeTask2
+ * \brief       Task to handle the strategy
+ *
+ * \param[in]   None
+ * \return      None
+ */
+static void vNodeTask2(void* pvParameters )
+{
+    /* endless */
+    for(;;)
+    {
+        node_task_2->node_function(&node_task_2->param,&state_node_2);
+    }
+}
+
+
+/**
+ * \fn          vTrackEnemy
+ * \brief       Track the enemys-position in an simple grid array
+ *
+ * \param[in]   id      CAN message ID
+ * \param[in]   data    CAN message data (here, ELP)
+ * \return      None
+ */
+static void vTrackEnemy(uint16_t id, CAN_data_t* data)
+{
+    /* locale variable */
+    uint8_t i;
+
+    /* data check  todo mit Antriebklaeren*/
+
+    /* check if the position within the grid and not on the frame */
+    if((data->elp_y/(ENEMY_GRID_SIZE_Y*1000)) < (PLAYGROUND_HEIGH/ENEMY_GRID_SIZE_Y-1) &&
+            (data->elp_y/(ENEMY_GRID_SIZE_Y*1000)) > 0 &&
+            (data->elp_x/(ENEMY_GRID_SIZE_X*1000)) > 0 &&
+            (data->elp_x/(ENEMY_GRID_SIZE_X*1000)) < (PLAYGROUND_WIDTH/ENEMY_GRID_SIZE_X-1))
+    {
+        /* set center weight */
+        enemey_position[(int)(data->elp_y/(ENEMY_GRID_SIZE_Y*1000))][(int)(data->elp_x/(ENEMY_GRID_SIZE_X*1000))] += ENEMY_GRID_CENTER_WEIGHT;
+
+        /* set upper frame edge */
+        for(i=0; i<3; i++)
+        {
+            enemey_position[(int)(data->elp_y/(ENEMY_GRID_SIZE_Y*1000)-1)][(int)(data->elp_x/(ENEMY_GRID_SIZE_X*1000)-1+i)] += ENEMY_GRID_FRAME_WEIGHT;
+        }
+
+        /* set deeper frame edge */
+        for(i=0; i<3; i++)
+        {
+            enemey_position[(int)(data->elp_y/(ENEMY_GRID_SIZE_Y*1000)+1)][(int)(data->elp_x/(ENEMY_GRID_SIZE_X*1000)-1+i)] += ENEMY_GRID_FRAME_WEIGHT;
+        }
+
+        /* set left frame edge */
+        enemey_position[(int)(data->elp_y/(ENEMY_GRID_SIZE_Y*1000))][(int)(data->elp_x/(ENEMY_GRID_SIZE_X*1000)-1)] += ENEMY_GRID_FRAME_WEIGHT;
+
+        /* set right frame edge */
+        enemey_position[(int)(data->elp_y/(ENEMY_GRID_SIZE_Y*1000))][(int)(data->elp_x/(ENEMY_GRID_SIZE_X*1000)+1)] += ENEMY_GRID_FRAME_WEIGHT;
+
+    }
+}
+
 
 /**
  * @}
--- Template/src/application/Strategy.h
+++ Template/src/application/Strategy.h
@@ -1,6 +1,6 @@
 /**
  * \file    Strategy.h
- * \author  meert1
+ * \author  gross10
  * \date    2013-11-29
  *
  * \version 1.0
@@ -21,13 +21,23 @@
 
 /* exported typedef -----------------------------------------------------------*/
 
-
 /* exported define ------------------------------------------------------------*/
 /* Strategy-task RTOS configuration */
-#define STRATEGY_TASK_NAME          "Strategy Task"
-#define STRATEGY_QUEUE_LENGTH       20 /*!< Size of the message queues */
-#define STRATEGY_STACK_SIZE         configMINIMAL_STACK_SIZE /*!< size of the receive and transmit task */
-#define STRATEGY_TASK_PRIORITY      (configMAX_PRIORITIES - 3UL) /*!< priority 3 */
+#define STRATEGY_SCHEDULER_TASK_NAME        "Strategy Task"
+#define STRATEGY_NODE_TASK_1_NAME           "Node Task 1"
+#define STRATEGY_NODE_TASK_2_NAME           "Node Task 2"
+#define STRATEGY_QUEUE_LENGTH               20 /*!< Size of the message queues */
+#define STRATEGY_STACK_SIZE                 configMINIMAL_STACK_SIZE /*!< size of the receive and transmit task */
+#define STRATEGY_SCHEDULER_TASK_PRIORITY    (configMAX_PRIORITIES - 3UL) /*!< priority 3 */
+#define STRATEGY_NODE_TASK_PRIORITY         (configMAX_PRIORITIES - 4UL) /*!< priority 2 */
+
+/* node and gamme configurations */
+#define PLAYGROUND_WIDTH            3 /*[m]*/
+#define PLAYGROUND_HEIGH            2 /*[m]*/
+#define ENEMY_GRID_SIZE_X           0.1 /*[m]*/
+#define ENEMY_GRID_SIZE_Y           0.1 /*[m]*/
+#define ENEMY_GRID_CENTER_WEIGHT    1
+#define ENEMY_GRID_FRAME_WEIGHT     0.5
 
 
 /* exported macro -------------------------------------------------------------*/
