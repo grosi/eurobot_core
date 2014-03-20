52a815d1389da5573ef04f94a04d211f987069ab
Simon Grossenbacher <grosi.mail@gmail.com>
2014-03-20 19:12:33 +0100

    now we have 7 different task priorities in our system

--- FreeRTOS/FreeRTOS/Config/FreeRTOSConfig.h
+++ FreeRTOS/FreeRTOS/Config/FreeRTOSConfig.h
@@ -98,7 +98,7 @@ extern uint32_t SystemCoreClock;
 #define configUSE_TICK_HOOK				0
 #define configCPU_CLOCK_HZ				( SystemCoreClock )
 #define configTICK_RATE_HZ				( ( portTickType ) 1000 )
-#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 6 )
+#define configMAX_PRIORITIES			( ( unsigned portBASE_TYPE ) 7 )
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 130 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 75 * 1024 ) )
 #define configMAX_TASK_NAME_LEN			( 10 )
