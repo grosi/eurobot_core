8d47c10c851c8c67b2d503c35cabde7fecfc5053
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2014-04-03 17:27:45 +0200

    Cosmetics

--- Template/src/application/nodes/FrescoNode.c
+++ Template/src/application/nodes/FrescoNode.c
@@ -94,7 +94,7 @@ void doFrescoNode(node_param_t* param) {
 		/* Get number of frescos still in robot */
 		n_frescos_present = getSensor_Fresco_1() + getSensor_Fresco_2();
 
-		/* Retry if there are still frescos in the robot and another retry is allowed (max. not reached). */
+	/* Retry if there are still frescos in the robot and another retry is allowed (max. not reached). */
 	} while(n_frescos_present && (n_fresco_attempts <= FRESCO_MAX_RETRIES));
 
 	/* Report status */
