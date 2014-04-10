b7a7a9f222986d40d36646ed0c8e12bd9345414d
Simon Grossenbacher <grosi.mail@gmail.com>
2014-04-10 21:37:44 +0200

    display function are ported to core-node (not tested)

--- Template/src/application/system/RoboSetup.c
+++ Template/src/application/system/RoboSetup.c
@@ -1,6 +1,6 @@
 /**
  * \file    RoboSetup.c
- * \author  gross10
+ * \author  haldj3
  * \date    2014-03-21
  *
  * \version 1.0
@@ -23,17 +23,57 @@
 #include "RoboRun.h" /* next state if this one is completed successfully */
 #include "RoboSetup.h"
 
+/* lib */
+#include "lib/button.h"
+#include "lib/display.h"
+
+
 /* Private typedef -----------------------------------------------------------*/
+/**
+ * \brief menu typedef
+ */
+typedef struct
+{
+    uint8_t title[MAX_NUMBER_COLUMN+1];
+    uint8_t byte0[2];
+    uint8_t byte15[2];
+    uint8_t opt1[MAX_NUMBER_COLUMN+1];
+    const uint8_t pos1;                         // 0... (MAX_NUMBER_COLUMN-1)
+    uint8_t opt2[MAX_NUMBER_COLUMN+1];    // will change in an adjusting menu
+    const uint8_t pos2;
+    uint8_t opt3[MAX_NUMBER_COLUMN+1];
+    const uint8_t pos3;
+    uint8_t cursor_position;
+    uint8_t max_result;
+    uint8_t result;
+    uint8_t confirmed;                          /*!< FALSE = not yet confirmed, TRUE = confirmed */
+} menu_t;
+
+
+/**
+ * \brief type of menu
+ * there are two types of menus
+ */
+typedef enum
+{
+    SELECTION_MENU = 0,
+    ADJUSTING_MENU
+} menu_type_t;
+
+
+/**
+ * \brief states of the statemaschine
+ */
 typedef enum
 {
     TEAMCOLOR = 0,
     ENEMY_QUANTITY,
-    CONDEDERATE_QUNATITY,
-    ENEMY_1_SIZE,
-    ENEMY_2_SIZE,
-    START_NODE,
-    SET_STARTCONFIG
-}robo_setup_t;
+    FRIEND_QUANTITY,
+    ENEMY_SIZE1,
+    ENEMY_SIZE2,
+    STARTNODE,
+    SETUP_FINISHED
+}menu_current_t;
 
 /* Private define ------------------------------------------------------------*/
 
@@ -42,11 +82,143 @@ typedef enum
 
 
 /* Private variables ---------------------------------------------------------*/
+/**
+ * \brief menu-architecture
+ */
+/* TEAMCOLOR */
+menu_t teamcolor =
+{
+    .title = "TEAMCOLOR",
+    .byte0 = "<",
+    .byte15 = ">",
+    .opt1 = "RED",
+    .pos1 = 2,
+    .opt2 = "YELLOW",
+    .pos2 = 8,
+    .opt3 = "",
+    .pos3 = 16, /*!< pos3 have to be >= 16 if not used! */
+    .cursor_position = SETUP_TEAMCOLOR_CURSOR_DEFAULT,
+    .max_result = 1,
+    .result = SETUP_TEAMCOLOR_RESULT_DEFAULT, // TODO RED = 0, YELLOW = 1
+    .confirmed = FALSE
+};
+
+/* FRIEND QUANTITY */
+menu_t friend_quantity =
+{
+    .title = "FRIEND QUANTITY",
+    .byte0 = "<",
+    .byte15 = ">",
+    .opt1 = "0",
+    .pos1 = 5,
+    .opt2 = "1",
+    .pos2 = 10,
+    .opt3 = "",
+    .pos3 =  16, /*!< pos3 have to be >= 16 if not used! */
+    .cursor_position = SETUP_FRIEND_CURSOR_DEFAULT,
+    .max_result = 1,
+    .result = SETUP_FRIEND_RESULT_DEFAULT, // TODO 0, 1
+    .confirmed = FALSE
+};
+
+/* ENEMY QUANTITY */
+menu_t enemy_quantity =
+{
+    .title = "ENEMY QUANTITY",
+    .byte0 = "<",
+    .byte15 = ">",
+    .opt1 = "0",
+    .pos1 = 3,
+    .opt2 = "1",
+    .pos2 = 7,
+    .opt3 = "2",
+    .pos3 = 11,
+    .cursor_position = SETUP_ENEMY_CURSOR_DEFAULT,
+    .max_result = 2,
+    .result = SETUP_ENEMY_RESULT_DEFAULT,    // 0, 1, 2
+    .confirmed = FALSE
+};
+
+/* ENEMY 1 SIZE*/
+menu_t enemy_size1 =
+{
+    .title = "ENEMY SIZE1 [cm]",
+    .byte0 = "-",
+    .byte15 = "+",
+    .opt1 = "0...",
+    .pos1 =1,
+    .opt2 = SETUP_ENEMY_1_OPTION,
+    .pos2 = 7,
+    .opt3 = "..50",
+    .pos3 = 11,
+    .cursor_position = SETUP_ENEMY_1_CURSOR_DEFAULT,
+    .max_result = 50,
+    .result = SETUP_ENEMY_1_RESULT_DEFAULT,    // 0... 50
+    .confirmed = FALSE
+};
+
+/* ENEMY 2 SIZE*/
+menu_t enemy_size2 =
+{
+    .title = "ENEMY SIZE1 [cm]",
+    .byte0 = "-",
+    .byte15 = "+",
+    .opt1 = "0...",
+    .pos1 =1,
+    .opt2 = SETUP_ENEMY_2_OPTION,
+    .pos2 = 7,
+    .opt3 = "..50",
+    .pos3 = 11,
+    .cursor_position = SETUP_ENEMY_2_CURSOR_DEFAULT,
+    .max_result = 50,
+    .result = SETUP_ENEMY_2_RESULT_DEFAULT,    // 0... 50
+    .confirmed = FALSE
+};
+
+/* STARTNODE */
+menu_t startnode =
+{
+    .title = "STARTNODE",
+    .byte0 = "-",
+    .byte15 = "+",
+    .opt1 = "0...",
+    .pos1 = 1,
+    .opt2 = SETUP_STARTNODE_OPTION,
+    .pos2 = 7,
+    .opt3 = "..??",
+    .pos3 = 11,
+    .cursor_position = SETUP_STARTNODE_CURSOR_DEFAULT,
+    .max_result = 50,  // ?
+    .result = SETUP_STARTNODE_RESULT_DEFAULT,    // 0... ??
+    .confirmed = FALSE
+};
+
+/* SETUP FINISHED */
+menu_t setup_finished =
+{
+    .title = "SETUP FINISHED?",
+    .byte0 = "<",
+    .byte15 = ">",
+    .opt1 = "NO",
+    .pos1 = 4,
+    .opt2 = "YES",
+    .pos2 = 9,
+    .opt3 = "",
+    .pos3 = 16,    // pos3 have to be >= 16 if not used!
+    .cursor_position = SETUP_SETUPFINISH_CURSOR_DEFAULT,
+    .max_result = 1,
+    .result = SETUP_SETUPFINISH_RESULT,    // 0 = NO, 1 = YES
+    .confirmed = FALSE
+};
+
+/* RTOS */
 xSemaphoreHandle sSyncRoboSetupELP = NULL;
 
 
 /* Private function prototypes -----------------------------------------------*/
-
+static void write_current_menu(menu_t **);
+static void menu_handler(menu_t *, uint8_t, uint8_t, uint8_t, menu_type_t);
+static void setRoboSetup2Default();
 
 /* Private functions ---------------------------------------------------------*/
 
@@ -62,6 +234,9 @@ void initRoboSetupState()
     /* create semaphore for elp task synchronisation */
     vSemaphoreCreateBinary(sSyncRoboSetupELP); /* is given 1 second after startconfigs are transmitted */
     xSemaphoreTake(sSyncRoboSetupELP,0); /* take the semaphore */
+
+    /* hw modules */
+    initUserPanelButtons();
 }
 
 
@@ -74,18 +249,293 @@ void initRoboSetupState()
  */
 void runRoboSetupState(portTickType* tick)
 {
-    static robo_setup_t state = TEAMCOLOR;
+    /* local variables */
+    static menu_current_t current_menu = TEAMCOLOR;
 
-    //TODO
-    setConfigRoboRunState(1,GIP_TEAMCOLOR_YELLOW,GIP_ENEMY_QUANTITY_1);
+//    uint8_t welcome_message[] = WelcomeMessage;
+//    uint8_t game_message_top[] = GameMessageTop;
+//    uint8_t game_message_bottom[] = GameMessageBotton;
 
-    resetTimer();
-    startTimer(); /* start game timer */
+    /* write the welcome message */
+//    LCD_line_mode_one();
+//    LCD_write_string(0, 0, welcome_message, TRUE);
+//    vTaskDelayUntil( &xLastFlashTime, WelcomeMessageDelay / portTICK_RATE_MS);    // wait 3s
+//    LCD_line_mode_two();
 
-    system_state = runRoboRunState;
+    switch(current_menu)
+    {
+        case TEAMCOLOR:
+            menu_handler(&teamcolor, 0, 1, 1, SELECTION_MENU);    // res1 = 0 (RED), res2 = 1 (YELLOW), res3 = res2 (not used), mode = selection-mode
+            if (teamcolor.confirmed == TRUE) current_menu = FRIEND_QUANTITY;
+            break;
+
+        case FRIEND_QUANTITY:
+            menu_handler(&friend_quantity, 0, 1, 1, SELECTION_MENU);    // res1 = 0, res2 = 1, res3 = res2 (not used), mode = selection-mode
+            if (friend_quantity.confirmed == TRUE) current_menu = ENEMY_QUANTITY;
+            break;
+
+        case ENEMY_QUANTITY:
+            menu_handler(&enemy_quantity, 0, 1, 2, SELECTION_MENU);    // res1 = 0, res2 = 1, res3 = 2, mode = selection-mode
+            if(enemy_quantity.confirmed == TRUE)
+            {
+                if(enemy_quantity.result >= 1)
+                {
+                    current_menu = ENEMY_SIZE1;
+                }
+                else
+                {
+                    current_menu = STARTNODE;
+                }
+            }
+            break;
+
+        case ENEMY_SIZE1:
+            menu_handler(&enemy_size1, 0, 0, 0, ADJUSTING_MENU);    // res1-3 = 0 (not used), mode = adjusting-mode
+            if (enemy_size1.confirmed == TRUE)
+            {
+                current_menu = ENEMY_SIZE2;
+            }
+            break;
+
+        case ENEMY_SIZE2:
+            menu_handler(&enemy_size2, 0, 0, 0, ADJUSTING_MENU);    // res1-3 = 0 (not used), mode = adjusting-mode
+            if (enemy_size2.confirmed == TRUE) current_menu = STARTNODE;
+            break;
+
+        case STARTNODE:
+            menu_handler(&startnode, 0, 0, 0, ADJUSTING_MENU);    // res1-3 = 0 (not used), mode = adjusting-mode
+            if (startnode.confirmed == TRUE) current_menu = SETUP_FINISHED;
+            break;
+
+        case SETUP_FINISHED:
+            menu_handler(&setup_finished, 0, 1, 1, SELECTION_MENU);    // res1 = 0 (NO), res2 = 1 (YES), res3 = res2 (not used), mode = selection-mode
+            if(setup_finished.confirmed == TRUE)
+            {
+                /* if confirmed with YES... */
+                if(setup_finished.result == 1)
+                {
+                    /* ...reset cursor */
+                    LCD_set_cursor(0, 0, 0);
+
+                    /* ...write game message */
+//                    LCD_write_string(0, 0, game_message_top, TRUE);
+//                    LCD_write_string(MAX_NUMBER_ROW-1, 0, game_message_bottom, TRUE);
+
+                    /* ...send the results */
+                    setConfigRoboRunState(1,GIP_TEAMCOLOR_YELLOW,GIP_ENEMY_QUANTITY_1);
+                    resetTimer();
+                    //...
+
+                    /* ...wait */
+//                    vTaskDelayUntil( &xLastFlashTime, 10000 / portTICK_RATE_MS);    // wait 10s
+
+                    /* ...reset setup to its defaults */
+                    current_menu = TEAMCOLOR;
+                    setRoboSetup2Default();
+                }
+                else
+                {
+                    current_menu = TEAMCOLOR;
+                }
+            }
+            break;
+    }
+
+    /* wait TODO*/
+    vTaskDelayUntil(tick, SETUP_BUTTOM_DELAY / portTICK_RATE_MS);
 
 
+//    static robo_setup_t state = TEAMCOLOR;
+//
+//    //TODO
+//    setConfigRoboRunState(1,GIP_TEAMCOLOR_YELLOW,GIP_ENEMY_QUANTITY_1);
+//
+//    resetTimer();
+//    startTimer(); /* start game timer */
+//
+//    system_state = runRoboRunState;
 }
+
+
+/**
+ * \fn      write_current_menu
+ * \brief   writes the given menu to the display
+ *
+ * \param[in]   **current_menu   address of the address of the current_menu
+ * \return  None
+ */
+static void write_current_menu(menu_t **current_menu)
+{
+    /* write title */
+    LCD_write_string(0, 0, (*current_menu)->title, TRUE);
+
+    /* write byte0 */
+    LCD_write_string(MAX_NUMBER_ROW-1, 0, (*current_menu)->byte0, TRUE);
+
+    /* write options */
+    LCD_write_string(MAX_NUMBER_ROW-1, (*current_menu)->pos1, (*current_menu)->opt1, TRUE);
+    LCD_write_string(MAX_NUMBER_ROW-1, (*current_menu)->pos2, (*current_menu)->opt2, TRUE);
+    LCD_write_string(MAX_NUMBER_ROW-1, (*current_menu)->pos3, (*current_menu)->opt3, TRUE);
+
+    /* write byte15 */
+    LCD_write_string(MAX_NUMBER_ROW-1, MAX_NUMBER_COLUMN-1, (*current_menu)->byte15, TRUE);
+
+    /* set cursor */
+    LCD_set_cursor(MAX_NUMBER_ROW-1, (*current_menu)->cursor_position, DISPLAY_CURSOR_MODE);
+}
+
+
+/**
+ * \fn      menu_handler
+ * \brief   checks the buttons, controls the given menu and saves the result
+ *
+ * \param[in]   *current_menu   address of the current_menu (&menu)
+ *              res1            possible result1
+ *              res2            possible result2
+ *              res3            possible result3
+ *              mode            0 = selection-mode (< ... >)
+ *                              1 = adjusting-mode (- ... +)
+ * \return  None
+ */
+static void menu_handler(menu_t *current_menu, uint8_t res1, uint8_t res2, uint8_t res3, menu_type_t mode)
+{
+//    portTickType xLastFlashTime;
+//
+//    /* We need to initialize xLastFlashTime prior to the first call to
+//    vTaskDelayUntil(). */
+//    xLastFlashTime = xTaskGetTickCount();
+
+    write_current_menu(&current_menu);
+
+    /* wait some time until checking the buttons again */
+//    vTaskDelayUntil(&xLastFlashTime, MenuDelay / portTICK_RATE_MS);
+
+    if(!getUserPanelButton_2())    // leave the menu if button 2 is pushed
+    {
+        switch(mode)
+        {
+            case SELECTION_MENU:
+                /* update cursor */
+                LCD_set_cursor(MAX_NUMBER_ROW-1, current_menu->cursor_position, DISPLAY_CURSOR_MODE);
+
+                /* check if button 1 is pushed */
+                if (getUserPanelButton_1())
+                {
+                    /* set new cursor position and result if the cursor is not at pos1 */
+                    if(current_menu->cursor_position >= current_menu->pos2)        // check pos2 & pos3
+                    {
+                        if(current_menu->cursor_position == current_menu->pos3)    // check pos3
+                        {
+                            current_menu->cursor_position = current_menu->pos2;
+                            current_menu->result = res2;
+                        }
+                        else
+                        {
+                            current_menu->cursor_position = current_menu->pos1;
+                            current_menu->result = res1;
+                        }
+                    }
+                }
+
+                /* check if button 3 is pushed */
+                if (getUserPanelButton_3())
+                {
+                    /* set new cursor position and result if the cursor is not at pos3 */
+                    if(current_menu->cursor_position <= current_menu->pos2)        // check pos1 & pos2
+                    {
+                        if(current_menu->cursor_position == current_menu->pos1)    // check pos1
+                        {
+                            current_menu->cursor_position = current_menu->pos2;
+                            current_menu->result = res2;
+                        }
+                        else if(current_menu->pos3 < MAX_NUMBER_COLUMN)   // check if pos3 is used
+                        {
+                            current_menu->cursor_position = current_menu->pos3;
+                            current_menu->result = res3;
+                        }
+                    }
+                }
+                break;
+
+            case ADJUSTING_MENU:
+                /* turn off cursor */
+                LCD_set_cursor(0, 0, 0);
+
+                /* check if button 1 is pushed and result > 0 */
+                if (getUserPanelButton_1() && current_menu->result > 0)
+                {
+                    current_menu->result--;    // decrease result
+                }
+
+                /* check if button 3 is pushed and result < max_result */
+                if (getUserPanelButton_3() && current_menu->result < current_menu->max_result)
+                {
+                    current_menu->result++;    // increase result
+                }
+
+                /* update display (option 2) */
+                current_menu->opt2[0] = ('0' + ((current_menu->result)/10));
+                current_menu->opt2[1] = ('0' + ((current_menu->result)%10));
+                current_menu->opt2[2] = '\0';
+                LCD_write_string(MAX_NUMBER_ROW-1, current_menu->pos2, current_menu->opt2, FALSE);
+                break;
+        }
+    }
+    else
+    {
+        /* confirm chosen result to get into the next menu */
+        current_menu->confirmed = TRUE;
+    }
+}
+
+
+/**
+ * \fn      setRoboSetup2Default
+ * \brief   resets the whole setup to its defaults
+ *
+ * \param[in]   None
+ * \return  None
+ */
+static void setRoboSetup2Default()
+{
+    teamcolor.cursor_position = SETUP_TEAMCOLOR_CURSOR_DEFAULT;
+    teamcolor.result = SETUP_TEAMCOLOR_RESULT_DEFAULT;
+    teamcolor.confirmed = FALSE;
+
+    friend_quantity.cursor_position = SETUP_FRIEND_CURSOR_DEFAULT;
+    friend_quantity.result = SETUP_FRIEND_RESULT_DEFAULT;
+    friend_quantity.confirmed = FALSE;
+
+    enemy_quantity.cursor_position = SETUP_ENEMY_CURSOR_DEFAULT;
+    enemy_quantity.result = SETUP_ENEMY_RESULT_DEFAULT;
+    enemy_quantity.confirmed = FALSE;
+
+    enemy_size1.opt2[0] = SETUP_ENEMY_1_OPTION_CH1;
+    enemy_size1.opt2[1] = SETUP_ENEMY_1_OPTION_CH2;
+    enemy_size1.opt2[2] = '\0';
+    enemy_size1.cursor_position = SETUP_ENEMY_1_CURSOR_DEFAULT;
+    enemy_size1.result = SETUP_ENEMY_1_RESULT_DEFAULT;
+    enemy_size1.confirmed = FALSE;
+
+    enemy_size2.opt2[0] = SETUP_ENEMY_2_OPTION_CH1;
+    enemy_size2.opt2[1] = SETUP_ENEMY_2_OPTION_CH2;
+    enemy_size2.opt2[2] = '\0';
+    enemy_size2.cursor_position = SETUP_ENEMY_2_CURSOR_DEFAULT;
+    enemy_size2.result = SETUP_ENEMY_2_RESULT_DEFAULT;
+    enemy_size2.confirmed = FALSE;
+
+    startnode.opt2[0] = SETUP_STARTNODE_OPTION_CH1;
+    startnode.opt2[1] = SETUP_STARTNODE_OPTION_CH2;
+    startnode.opt2[2] = '\0';
+    startnode.cursor_position = SETUP_STARTNODE_CURSOR_DEFAULT;
+    startnode.result = SETUP_STARTNODE_RESULT_DEFAULT;
+    startnode.confirmed = FALSE;
+
+    setup_finished.cursor_position = SETUP_SETUPFINISH_CURSOR_DEFAULT;
+    setup_finished.result = SETUP_SETUPFINISH_RESULT;
+    setup_finished.confirmed = FALSE;
+}
+
 /**
  * @}
  */
--- Template/src/application/system/RoboSetup.h
+++ Template/src/application/system/RoboSetup.h
@@ -21,6 +21,49 @@
 
 
 /* exported define ------------------------------------------------------------*/
+/* delays */
+#define SETUP_BUTTOM_DELAY 100    // sampling delay of the buttons in ms
+//#define MenuDelay 1000     // delay to ignore the buttons after switching a menu in ms
+//#define WelcomeMessageDelay 3000    // delay to read the welcome message
+
+/* cursor */
+#define DISPLAY_CURSOR_MODE 2      // 1 = cursor, 2 = blink-function, 3 = both
+
+/* messages */
+//#define WelcomeMessage " W E L C O M E !"    // max 16 characters
+//#define GameMessageTop "  ROBO SAPIENS  "
+//#define GameMessageBotton " BERNENSIS FTW! "
+
+/* default values of the menus */
+#define SETUP_TEAMCOLOR_CURSOR_DEFAULT 2
+#define SETUP_TEAMCOLOR_RESULT_DEFAULT 0 // RED = 0, YELLOW = 1
+
+#define SETUP_FRIEND_CURSOR_DEFAULT 10
+#define SETUP_FRIEND_RESULT_DEFAULT 1 // 0, 1
+
+#define SETUP_ENEMY_CURSOR_DEFAULT 7
+#define SETUP_ENEMY_RESULT_DEFAULT 1 // 0, 1, 2
+
+#define SETUP_ENEMY_1_OPTION "25"
+#define SETUP_ENEMY_1_OPTION_CH1 '2' // a character is easier to read for the setup reset
+#define SETUP_ENEMY_1_OPTION_CH2 '5'
+#define SETUP_ENEMY_1_CURSOR_DEFAULT 8
+#define SETUP_ENEMY_1_RESULT_DEFAULT 25 // 0... 50
+
+#define SETUP_ENEMY_2_OPTION "25"
+#define SETUP_ENEMY_2_OPTION_CH1 '2' // a character is easier to read for the setup reset
+#define SETUP_ENEMY_2_OPTION_CH2 '5'
+#define SETUP_ENEMY_2_CURSOR_DEFAULT 8
+#define SETUP_ENEMY_2_RESULT_DEFAULT 25 // 0... 50
+
+#define SETUP_STARTNODE_OPTION "00"
+#define SETUP_STARTNODE_OPTION_CH1 '0'
+#define SETUP_STARTNODE_OPTION_CH2 '0'
+#define SETUP_STARTNODE_CURSOR_DEFAULT 8
+#define SETUP_STARTNODE_RESULT_DEFAULT 0
+
+#define SETUP_SETUPFINISH_CURSOR_DEFAULT 4
+#define SETUP_SETUPFINISH_RESULT 0 // 0 = No, 1 = YES
 
 
 /* exported macro -------------------------------------------------------------*/
