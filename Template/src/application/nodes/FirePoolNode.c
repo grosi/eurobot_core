91a97042f63fa93ea4f1942ec567d6d9e608ef2f
Simon Grossenbacher <grosi.mail@gmail.com>
2014-05-13 13:47:02 +0200

    first implementation ready for testing

--- Template/src/application/nodes/FirePoolNode.c
+++ Template/src/application/nodes/FirePoolNode.c
@@ -22,6 +22,7 @@
 #include "FirePoolNode.h"
 /* lib */
 #include "lib/air.h"
+#include "lib/sensor.h"
 #include "lib/servo.h"
 
 /* Private typedef -----------------------------------------------------------*/
@@ -36,9 +37,129 @@
 
 
 /* Private function prototypes -----------------------------------------------*/
+static void releasePool(uint8_t);
+static uint8_t takePool(uint16_t, uint16_t, uint16_t, volatile game_state_t*);
 
 
 /* Private functions ---------------------------------------------------------*/
+
+
+
+
+/**
+ * \fn      releasePool
+ * \brief   take the pool free
+ *
+ * \param[in]   air     1:=air still on; 0:=air off
+ */
+static void releasePool(uint8_t air)
+{
+    /* local variable */
+    volatile uint16_t servo_pos; /* Variable to set the servo position step by step */
+
+    if(air)
+    {
+        setAir(AIR_OFF);
+    }
+
+    /* Move the sucker servo up, step by step */
+    servo_pos = getServo_1(); /* Current position */
+    while(servo_pos < (SERVO_POS_AIR_UP+SERVO_AIR_STEP))
+    {
+        /* Decrement servo position by step size */
+        servo_pos += SERVO_AIR_STEP;
+
+        /* Check if it's the last step */
+        if(servo_pos > SERVO_POS_AIR_UP)
+        {
+            /* Set the final servo position without over-rotating */
+            setServo_1(SERVO_POS_AIR_UP);
+        }
+        else
+        {
+            /* Set the new servo position */
+            setServo_1(servo_pos);
+        }
+        /* Wait some time while servo moves */
+        vTaskDelay(SERVO_AIR_STEP_DELAY / portTICK_RATE_MS);
+    }
+}
+
+
+/**
+ * \fn      takePool
+ * \brief   take the pool or the next fire
+ *
+ * \param[in]   param   node parameters
+ * \param[in]   game_state  current game states
+ *
+ * \retval  0   error
+ * \retval  1   success
+ */
+static uint8_t takePool(uint16_t x, uint16_t y, uint16_t angle, volatile game_state_t* game_state)
+{
+    /* local variable */
+    volatile uint16_t servo_pos; /* Variable to set the servo position step by step */
+    volatile uint16_t approach_counter = 0;
+    uint8_t success;
+
+    /* Drive closer to the pool */
+    if(checkDrive(x,y,angle,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_FORWARD,game_state))
+    {
+        while(!getSensor_Fire_Pool_Left() && !getSensor_Fire_Pool_Left() && approach_counter < FIRE_POOL_APPROACH_TIME)
+        {
+            /* Wait some time before next check */
+            vTaskDelay(FIRE_POOL_DRIVECHECK_DELAY / portTICK_RATE_MS);
+            approach_counter += FIRE_POOL_DRIVECHECK_DELAY;
+        }
+
+        /* Stop driving further */
+        txStopDrive();
+
+        /* check pool position */
+        if(getSensor_Fire_Pool_Left() && getSensor_Fire_Pool_Right())
+        {
+            /* Move the sucker servo down a bit, step by step */
+            servo_pos = getServo_1(); /* Current position */
+            while(servo_pos > (SERVO_POS_AIR_DOWN+SERVO_AIR_STEP) && !getSensor_Air())
+            {
+                /* Decrement servo position by step size */
+                servo_pos -= SERVO_AIR_STEP;
+
+                /* Check if it's the last step */
+                if(servo_pos < SERVO_POS_AIR_DOWN)
+                {
+                    /* Set the final servo position without over-rotating */
+                    setServo_1(SERVO_POS_AIR_DOWN);
+                }
+                else
+                {
+                    /* Set the new servo position */
+                    setServo_1(servo_pos);
+                }
+                /* Wait some time while servo moves  (a bit slower)*/
+                vTaskDelay(SERVO_AIR_STEP_DELAY*10 / portTICK_RATE_MS);
+            }
+
+            setAir(AIR_ON);
+
+            /* pool is at the right position and sucket */
+            success = 1;
+        }
+        else
+        {
+            /* no pool detected */
+            success = 0;
+        }
+    }
+    else
+    {
+        /*enemy is in front */
+        success = 0;
+    }
+
+    return success;
+}
 
 
 /**
@@ -50,92 +171,87 @@
  */
 void doFirePoolNode(node_param_t* param, volatile game_state_t* game_state)
 {
+    /* local variables */
+    uint8_t fire_count;
+    uint16_t x_pool, y_pool, angle_pool;
+    uint16_t x_heart, y_heart, angle_heart;
 
-    setAir(1);
-    vTaskDelay(2000);
-    setAir(0);
+    /* differ between the tow possible teamcolors */
+    if(game_state->teamcolor == TEAM_YELLOW)
+    {
+        x_pool = FIRE_POOL_HEARTPOOL_X_POSITION_YELLOW;
+        y_pool = FIRE_POOL_HEARTPOOL_Y_POSITION_YELLOW;
+        angle_pool = FIRE_POOL_HEARTPOOL_ANGLE_POSITION_YELLOW;
+        x_heart = FIRE_POOL_HEART_X_POSITION_YELLOW;
+        y_heart = FIRE_POOL_HEART_Y_POSITION_YELLOW;
+        angle_heart = FIRE_POOL_HEART_ANGLE_POSITION_YELLOW;
+    }
+    else
+    {
+        x_pool = FIRE_POOL_HEARTPOOL_X_POSITION_RED;
+        y_pool = FIRE_POOL_HEARTPOOL_Y_POSITION_RED;
+        angle_pool = FIRE_POOL_HEARTPOOL_ANGLE_POSITION_RED;
+        x_heart = FIRE_POOL_HEART_X_POSITION_RED;
+        y_heart = FIRE_POOL_HEART_Y_POSITION_RED;
+        angle_heart = FIRE_POOL_HEART_ANGLE_POSITION_RED;
+    }
 
-    setServo_1(SERVO_POS_AIR_DOWN);
-    vTaskDelay(2000);
-    setServo_1(SERVO_POS_AIR_THIRD_FIRE);
-    vTaskDelay(2000);
-    setServo_1(SERVO_POS_AIR_SECOND_FIRE);
-    vTaskDelay(2000);
-    setServo_1(SERVO_POS_AIR_FIRST_FIRE);
-    vTaskDelay(2000);
-    setServo_1(SERVO_POS_AIR_UP);
 
-    vTaskDelay(2000);
-    setServo_2(SERVO_POS_NET_LAUNCH);
-    vTaskDelay(2000);
-    setServo_2(SERVO_POS_NET_LOAD);
-    vTaskDelay(2000);
-//    /* local variables */
-//    /* Variable for CAN RX */
-//    CAN_data_t CAN_buffer;
-//    uint8_t CAN_ok = pdFALSE;
-//
-//    /* Activate rangefinder */
-//    vTaskResume(xRangefinderTask_Handle);
-//
-//    /* Move the separation all the way out */
-//    //setServo_1(SERVO_POS_FRESCO_OUT);
-//
-//    /* Wait some time while servo moves */
-//    vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
-//
-//    /* Drive through fire from NORTH */
-//    if(param->angle >= NODE_NORTH_MIN_ANGLE && param->angle <= NODE_NORTH_MAX_ANGLE)
-//    {
-//        txGotoXY(param->x, param->y-FIRE_NODE_DELTA_GO, param->angle, FIRE_NODE_SPEED, FIRE_NODE_BARRIER, GOTO_DRIVE_FORWARD);
-//
-//    }
-//    /* Drive through fire from EAST */
-//    else if(param->angle >= NODE_EAST_MIN_ANGLE && param->angle <= NODE_EAST_MAX_ANGLE)
-//    {
-//        txGotoXY(param->x-FIRE_NODE_DELTA_GO, param->y, param->angle, FIRE_NODE_SPEED, FIRE_NODE_BARRIER, GOTO_DRIVE_FORWARD);
-//    }
-//    /* Drive through fire from SOUTH */
-//    else if(param->angle >= NODE_SOUTH_MIN_ANGLE && param->angle <= NODE_SOUTH_MAX_ANGLE)
-//    {
-//        txGotoXY(param->x, param->y+FIRE_NODE_DELTA_GO, param->angle, FIRE_NODE_SPEED, FIRE_NODE_BARRIER, GOTO_DRIVE_FORWARD);
-//    }
-//    /* Drive through fire from WEST */
-//    else
-//    {
-//        txGotoXY(param->x+FIRE_NODE_DELTA_GO, param->y, param->angle, FIRE_NODE_SPEED, FIRE_NODE_BARRIER, GOTO_DRIVE_FORWARD);
-//    }
-//
-//    /* Receive GoTo confirmation */
-//    CAN_ok = xQueueReceive(qGotoConfirm, &CAN_buffer, FIRE_NODE_ACK_DELAY / portTICK_RATE_MS);
-//
-//    /* check CAN response */
-//    if(CAN_ok != pdTRUE)
-//    {
-////        /* Suspend rangefinder safely */
-////        suspendRangefinderTask();
-////
-////        param->node_state = GOTO_CAN_ERROR;
-////        return;
-//    }
-//
-//    //TODO add function to chek if there is any enemy or friend in my way while driving
-//
-//    /* Put seperation in after driving through the fire */
-//    //setServo_1(SERVO_POS_FRESCO_IN);
-//
-//    /* Wait some time while servo moves */
-//    vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
-//
-//    /* Fire has fallen */
-//    if(Rangefinder_flag_SeAlarmUS == 0){
-//
-//        param->node_state = NODE_FINISH_SUCCESS;
-//    }
-//
-//    /* Suspend rangefinder safely */
-//    suspendRangefinderTask();
+    /* try to get the fire-pool */
+    if(takePool(param->x, param->y - FIRE_POOL_APPROACH_DISTANCE - FIRE_POOL_APPROACH_OVERHEAD,
+            param->angle, game_state))
+    {
+        /* drive to the heart of fire */
+        if(checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state))
+        {
+            for(fire_count = 0; fire_count < 3; fire_count++)
+            {
+                /* move the sucker up with fire */
+                releasePool(1);
 
+                /* the second fire lies on the wrong side */
+                if(fire_count % 2)
+                {
+                    /* turn a bit */
+                    if(checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    releasePool(0);
+                }
+                else
+                {
+                    /* drive back 5cm*/
+                    if(checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
+                    /* drive to the front of heart*/
+                    if(checkDrive(x_heart-50,y_heart-50,angle_heart,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    /* moves sucker down */
+                    placeSucker(SERVO_POS_AIR_HEART);
+                    /* drive a bit forward (slowly) and shift the other fire back */
+                    if(checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    /* place fire and moves the sucker up */
+                    releasePool(0);
+                    /* drive a bit backwarts */
+                    if(checkDrive(x_heart-50,y_heart-50,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
+                }
+
+                if(fire_count < 2)
+                {
+                    /* drive back to the pool and take the next fire */
+                    if(checkDrive(x_pool-50,y_pool-50,angle_pool,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    if(takePool(x_pool, y_pool,angle_pool, game_state)){break;};
+                }
+            }
+            releasePool(0); /* be sure that the sucker is up and the air-system off */
+            param->node_state = NODE_FINISH_SUCCESS;
+        }
+        else
+        {
+            param->node_state = NODE_FINISH_SUCCESS;
+            releasePool(0);
+        }
+    }
+    else
+    {
+        param->node_state = NODE_FINISH_ERROR;
+    }
 }
 
 /**
--- Template/src/application/nodes/FirePoolNode.h
+++ Template/src/application/nodes/FirePoolNode.h
@@ -21,11 +21,26 @@
 
 
 /* exported define ------------------------------------------------------------*/
-//#define FIRE_NODE_DELTA_GO 250 /*!< [mm] */
-//#define FIRE_NODE_SPEED 100 /*!< [%] */
-//#define FIRE_NODE_BARRIER 0
-//#define FIRE_NODE_ACK_DELAY 20
-//#define FIRE_NODE_DRIVE_DELAY 500
+#define FIRE_POOL_APPROACH_DISTANCE                 100 /* Distance in mm to drive from the fire-pool node point to the pool */
+#define FIRE_POOL_APPROACH_OVERHEAD                 30 /* Additional Distance in mm to drive to the pool to make sure we are there */
+#define FIRE_POOL_APPROACH_SPEED                    30 /* Speed in percent to drive from node point to pool */
+#define FIRE_POOL_TRANSIT_SPEED                     100
+#define FIRE_POOL_APPROACH_TIME                     5000 /* Time in ms to wait while driving to pool */ //TODO
+#define FIRE_POOL_DRIVECHECK_DELAY                  10
+#define FIRE_POOL_MAX_RETRIES                       5
+#define FIRE_POOL_ACK_DELAY                         20
+#define FIRE_POOL_HEARTPOOL_X_POSITION_RED          250
+#define FIRE_POOL_HEARTPOOL_Y_POSITION_RED          410
+#define FIRE_POOL_HEARTPOOL_ANGLE_POSITION_RED      225
+#define FIRE_POOL_HEART_X_POSITION_RED              300
+#define FIRE_POOL_HEART_Y_POSITION_RED              180
+#define FIRE_POOL_HEART_ANGLE_POSITION_RED          225
+#define FIRE_POOL_HEARTPOOL_X_POSITION_YELLOW       2750
+#define FIRE_POOL_HEARTPOOL_Y_POSITION_YELLOW       410
+#define FIRE_POOL_HEARTPOOL_ANGLE_POSITION_YELLOW   320
+#define FIRE_POOL_HEART_X_POSITION_YELLOW           2700
+#define FIRE_POOL_HEART_Y_POSITION_YELLOW           180
+#define FIRE_POOL_HEART_ANGLE_POSITION_YELLOW       320
 
 /* exported macro -------------------------------------------------------------*/
 
