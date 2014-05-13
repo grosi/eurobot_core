742930dae41691b2c6bc2ecc825b0af36871ad9f
Simon Grossenbacher <grosi.mail@gmail.com>
2014-05-13 13:52:29 +0200

    fix some merge conflicts

--- Template/src/application/nodes/NodeConfig.h
+++ Template/src/application/nodes/NodeConfig.h
@@ -134,6 +134,12 @@ typedef struct
 /* Fire node */
 #define FIRE_APPROACH_DISTANCE  400
 
+/* Firewallnodes */
+#define FIRE_WALL_NODE_SPEED        30      /*!< [%] */
+#define FIRE_WALL_NODE_BARRIER      0
+#define FIRE_WALL_NODE_DRIVE_DELAY  500
+#define FIRE_WALL_NODE_DRIVE_BACK_DELAY     500
+
 
 /* exported macro -------------------------------------------------------------*/
 
