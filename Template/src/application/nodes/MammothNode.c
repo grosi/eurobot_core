a7bfc14ffcfb2403e2800fb5563c932714d572b0
Ben-Kaniobi <Ben_Kaniobi@gmx.net>
2014-05-14 09:13:27 +0200

    Removed rangefinder check in mammoth node

--- Template/src/application/nodes/MammothNode.c
+++ Template/src/application/nodes/MammothNode.c
@@ -49,42 +49,6 @@ volatile uint8_t Mammoth_flag_SeparationDone = 1;
 
 /**
  * \fn          doMammothNode
- * \brief       Moves the separation out if it's save to do so (checks rangefinder)
- * \note        Doesn't wait after the servo value is set. Also doesn't move the servo in again.
- *
- * \param       retry_delay      Delay in ms between retries
- * \param       retry_count_max  Max. number of retries (including first try)
- * \return      separation_done  1 if separation done, 0 if undone
- */
-uint8_t moveSeparationOutSavely(uint8_t retry_delay, uint8_t retry_count_max, volatile game_state_t* game_state) {
-
-	/* Variable to count number of retries */
-	uint8_t i = 0;
-
-	/* Check if object within separation range & if it could be a robot  */
-	while(Rangefinder_flag_SeAlarmUS && isRobotInFront(game_state)) {
-
-		/* Separation (fresco panel) space is blocked, wait or return */
-		if(i < retry_count_max) {
-
-			vTaskDelay(retry_delay / portTICK_RATE_MS);
-			i++;
-		}
-		else {
-
-			/* Max retries reached */
-			return 0;
-		}
-	}
-
-	/* Move the separation all the way out */
-	setServo_1(SERVO_POS_FRESCO_OUT);
-	return 1;
-}
-
-
-/**
- * \fn          doMammothNode
  * \brief       Tries to complete the mammoth node. Reports status.
  *
  * \param       param  node parameters
@@ -96,30 +60,12 @@ void doMammothNode(node_param_t* param, volatile game_state_t* game_state) {
 	/* Variable to set the servo position step by step */
 	volatile uint16_t servo_pos;
 
-	/* Activate rangefinder (needed to check separation space) */
-	vTaskResume(xRangefinderTask_Handle);
-
-	/* Check if on the previous run the separation couldn't be done because it was blocked too long */
-	if(!Mammoth_flag_SeparationDone) {
-
-		/* Retry it now */
-		Mammoth_flag_SeparationDone = moveSeparationOutSavely(SEPARATION_RETRY_DELAY, SEPARATION_MAX_RETRIES, game_state);
-		/* Don't continue if it's still not possible to move separation out */
-		if(!Mammoth_flag_SeparationDone) {
-
-			/* Report status */
-			param->node_state = NODE_FINISH_ERROR;
-			return;
-		}
-	}
-
-	/* Move the launcher servo all the way forward, in case it isn't already */
-	setServo_2(SERVO_POS_LAUNCHER_LOAD);
-	/* Wait some time while servo moves */
-	vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
-
 	/* Make sure the separation is all the way in */
 	setServo_1(SERVO_POS_FRESCO_IN);
+	/* Move the launcher servo all the way forward, in case it isn't already */
+	setServo_2(SERVO_POS_LAUNCHER_LOAD);
+	/* Wait some time while servos move */
+	vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
 
 	/* Move the launcher servo all the way back to launch the two loaded balls, step by step */
 	servo_pos = SERVO_POS_LAUNCHER_LOAD;  /* Current position */
@@ -171,18 +117,15 @@ void doMammothNode(node_param_t* param, volatile game_state_t* game_state) {
 		setServo_2(SERVO_POS_LAUNCHER_LOAD+SERVO_LAUNCHER_RELIEF);
 	}
 
-	/* Move the separation out if it's save to do so */
-	Mammoth_flag_SeparationDone = moveSeparationOutSavely(SEPARATION_RETRY_DELAY, SEPARATION_MAX_RETRIES, game_state);
+	/* Move the separation all the way out */
+	setServo_1(SERVO_POS_FRESCO_OUT);
 	/* Wait some time while servo moves */
 	vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
 
-	/* Make sure the separation is all the way in */
+	/* Move the separation all the way in */
 	setServo_1(SERVO_POS_FRESCO_IN);
 	/* Wait some time while servo moves */
 	vTaskDelay(SERVO_MOVING_DELAY / portTICK_RATE_MS);
-
-	/* Suspend rangefinder safely */
-	suspendRangefinderTask();
 
 	/* Report status */
 	param->node_state = NODE_FINISH_SUCCESS;
