ce0bffcd28898036e2c01e6a856e0a465f2258c4
Simon Grossenbacher <grosi.mail@gmail.com>
2014-05-17 10:31:00 +0200

    add the possibility to solve the task as member of the yellow team

--- Template/src/application/nodes/FirePoolNode.c
+++ Template/src/application/nodes/FirePoolNode.c
@@ -66,26 +66,6 @@ static void releasePool(uint8_t air)
 
     /* Move the sucker servo up, step by step */
     placeSucker(SERVO_POS_AIR_UP);
-//    servo_pos = getServo_1(); /* Current position */
-//    while(servo_pos < (SERVO_POS_AIR_UP+SERVO_AIR_STEP))
-//    {
-//        /* Decrement servo position by step size */
-//        servo_pos += SERVO_AIR_STEP;
-//
-//        /* Check if it's the last step */
-//        if(servo_pos > SERVO_POS_AIR_UP)
-//        {
-//            /* Set the final servo position without over-rotating */
-//            setServo_1(SERVO_POS_AIR_UP);
-//        }
-//        else
-//        {
-//            /* Set the new servo position */
-//            setServo_1(servo_pos);
-//        }
-//        /* Wait some time while servo moves */
-//        vTaskDelay(SERVO_AIR_STEP_DELAY / portTICK_RATE_MS);
-//    }
 }
 
 
@@ -176,23 +156,38 @@ void doFirePoolNode(node_param_t* param, volatile game_state_t* game_state)
 {
     /* local variables */
     uint8_t fire_count;
-    uint16_t x_pool, y_pool, angle_pool;
+    uint16_t x_pool, y_pool, angle_pool, x_pool_front, y_pool_front;
+    int16_t x_pool_approach_first, y_pool_approach_first, x_pool_approach_second, y_pool_approach_second;
     uint16_t x_heart, y_heart, angle_heart, x_heart_front, y_heart_front;
 
-    /* differ between the tow possible teamcolors */
+    /* differ between the two possible teamcolors */
     if(game_state->teamcolor == TEAM_YELLOW)
     {
         x_pool = FIRE_POOL_HEARTPOOL_X_POSITION_YELLOW;
         y_pool = FIRE_POOL_HEARTPOOL_Y_POSITION_YELLOW;
+        x_pool_front = FIRE_POOL_HEARTPOOL_FRONT_X_POSITION_YELLOW;
+        y_pool_front = FIRE_POOL_HEARTPOOL_FRONT_Y_POSITION_YELLOW;
+        x_pool_approach_first = FIRE_POOL_HEARTPOOL_APPROACH_FIRST_X_YELLOW;
+        y_pool_approach_first = FIRE_POOL_HEARTPOOL_APPROACH_FIRST_Y_YELLOW;
+        x_pool_approach_second = FIRE_POOL_HEARTPOOL_APPROACH_SECOND_X_YELLOW;
+        y_pool_approach_second = FIRE_POOL_HEARTPOOL_APPROACH_SECOND_Y_YELLOW;
         angle_pool = FIRE_POOL_HEARTPOOL_ANGLE_POSITION_YELLOW;
         x_heart = FIRE_POOL_HEART_X_POSITION_YELLOW;
         y_heart = FIRE_POOL_HEART_Y_POSITION_YELLOW;
+        x_heart_front = FIRE_POOL_HEART_FRONT_X_POSITION_YELLOW;
+        y_heart_front = FIRE_POOL_HEART_FRONT_Y_POSITION_YELLOW;
         angle_heart = FIRE_POOL_HEART_ANGLE_POSITION_YELLOW;
     }
     else
     {
         x_pool = FIRE_POOL_HEARTPOOL_X_POSITION_RED;
         y_pool = FIRE_POOL_HEARTPOOL_Y_POSITION_RED;
+        x_pool_front = FIRE_POOL_HEARTPOOL_FRONT_X_POSITION_RED;
+        y_pool_front = FIRE_POOL_HEARTPOOL_FRONT_Y_POSITION_RED;
+        x_pool_approach_first = FIRE_POOL_HEARTPOOL_APPROACH_FIRST_X_RED;
+        y_pool_approach_first = FIRE_POOL_HEARTPOOL_APPROACH_FIRST_Y_RED;
+        x_pool_approach_second = FIRE_POOL_HEARTPOOL_APPROACH_SECOND_X_RED;
+        y_pool_approach_second = FIRE_POOL_HEARTPOOL_APPROACH_SECOND_Y_RED;
         angle_pool = FIRE_POOL_HEARTPOOL_ANGLE_POSITION_RED;
         x_heart = FIRE_POOL_HEART_X_POSITION_RED;
         y_heart = FIRE_POOL_HEART_Y_POSITION_RED;
@@ -201,13 +196,13 @@ void doFirePoolNode(node_param_t* param, volatile game_state_t* game_state)
         angle_heart = FIRE_POOL_HEART_ANGLE_POSITION_RED;
     }
 
+
+    /* reset barrier */
     game_state->barrier &= ~(GOTO_FIRE_POOL_1_FORCE | GOTO_FIRE_POOL_2_FORCE);
 
 
     /* try to get the fire-pool */
-    //if(takePool(param->x, param->y - FIRE_POOL_APPROACH_DISTANCE - FIRE_POOL_APPROACH_OVERHEAD,
-	if(takePool(param->x - FIRE_POOL_APPROACH_DISTANCE - FIRE_POOL_APPROACH_OVERHEAD, param->y- FIRE_POOL_APPROACH_DISTANCE - FIRE_POOL_APPROACH_OVERHEAD,
-            param->angle, game_state))
+	if(takePool(param->x + x_pool_approach_first, param->y + y_pool_approach_first, param->angle, game_state))
     {
         /* drive to the heart of fire */
         if(checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state))
@@ -224,13 +219,13 @@ void doFirePoolNode(node_param_t* param, volatile game_state_t* game_state)
 					if(!checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
 					vTaskDelay(1000/portTICK_RATE_MS);
 					/* drive to the front of heart*/
-					if(!checkDrive(x_heart_front,y_heart_front,angle_heart,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
-					/* drive a bit forward (slowly) and shift the other fire back */
-					if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+					if(!checkDrive(x_heart_front,y_heart_front,angle_heart,FIRE_POOL_PLACE_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+					/* drive a bit forward */
+					if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_PLACE_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
 					/* place fire and moves the sucker up */
 					releasePool(0);
 					/* drive a bit backwarts */
-					if(!checkDrive(x_heart-50,y_heart-50,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
+					if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
 					vTaskDelay(1000/portTICK_RATE_MS);
                 }
                 else
@@ -239,24 +234,34 @@ void doFirePoolNode(node_param_t* param, volatile game_state_t* game_state)
                     if(!checkDrive(x_pool,y_pool,angle_pool,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
                     vTaskDelay(1000/portTICK_RATE_MS);
                     /* drive to the front of heart*/
-                    if(!checkDrive(x_heart_front,y_heart_front,angle_heart,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    if(!checkDrive(x_heart_front,y_heart_front,angle_heart,FIRE_POOL_PLACE_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
                     /* moves sucker down */
                     placeSucker(SERVO_POS_AIR_HEART);
-                    /* drive a bit forward (slowly) and shift the other fire back */
-                    if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+
+                    /* first fire with fullspeed */
+                    if(fire_count == 0)
+                    {
+                        /* drive a bit forward  */
+                        if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_PLACE_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    }
+                    /* third a bit slower */
+                    else
+                    {
+                        /* drive a bit forward (slowly) and shift the other fire back */
+                        if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_SHIFT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    }
                     /* place fire and moves the sucker up */
                     releasePool(0);
                     /* drive a bit backwarts */
-                    if(!checkDrive(x_heart-50,y_heart-50,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
+                    if(!checkDrive(x_heart,y_heart,angle_heart,FIRE_POOL_APPROACH_SPEED,GOTO_DRIVE_BACKWARD,game_state)){break;};
                     vTaskDelay(1000/portTICK_RATE_MS);
                 }
 
                 if(fire_count < 2)
                 {
                     /* drive back to the pool and take the next fire */
-                	//TODO Farbe unterscheiden
-                    if(!checkDrive(x_pool+150,y_pool,angle_pool,FIRE_POOL_TRANSIT_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
-                    if(!takePool(x_pool-100, y_pool,angle_pool, game_state)){break;};
+                    if(!checkDrive(x_pool + x_pool_front,y_pool+y_pool_front,angle_pool,FIRE_POOL_PLACE_SPEED,GOTO_DRIVE_FORWARD,game_state)){break;};
+                    if(!takePool(x_pool + x_pool_approach_second, y_pool,angle_pool+ y_pool_approach_second, game_state)){break;};
                 }
             }
             releasePool(0); /* be sure that the sucker is up and the air-system off */
