9ca6ffbc21f0f6f5271b29bd4b85dad8e5e6087e
reto.zurschmiede <reto.zu@gmail.com>
2014-05-22 15:16:30 +0200

    Fixed retval order

--- Template/src/application/nodes/NodeMisc.c
+++ Template/src/application/nodes/NodeMisc.c
@@ -188,6 +188,9 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
                      * else we may get the old time */
                     vTaskDelay(GOTO_STATERESP_DELAY / portTICK_RATE_MS);
 
+                    /* Success if not changed in do-while */
+                    retval = FUNC_SUCCESS;
+
                     do
                     {
                         /* Ask drive system for GoTo state */
@@ -245,8 +248,6 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
                         vTaskDelay(CAN_CHECK_DELAY/portTICK_RATE_MS);
                     }
                     while(estimated_GoTo_time > 0);
-
-                    retval = FUNC_SUCCESS;
                 }
                 else
                 {
@@ -280,6 +281,9 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
                 /* Wait at least GOTO_STATERESP_DELAY before asking for goto time for the first time,
                  * else we may get the old time */
                 vTaskDelay(GOTO_STATERESP_DELAY / portTICK_RATE_MS);
+
+                /* Success if not changed in do-while */
+                retval = FUNC_SUCCESS;
 
                 do
                 {
@@ -350,7 +354,7 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
                             txStopDrive();
 
 
-                            //TODO
+                            //v- TODO: Drive backward if enemy in range or not?
                             ////////////////////////////////////////////////////////////////////
                             /* make possible to break the node task for funny action */
 //							taskENTER_CRITICAL();
@@ -359,27 +363,21 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
 							{
 								/* Wait while driving */
 								vTaskDelay(DRIVE_BACK_TIME / portTICK_RATE_MS);
+
+	                            retval = FUNC_INCOMPLETE_LIGHT;
+	                            break;
 							}
 							else
 							{
 								retval = FUNC_ERROR;
+	                            break;
 							}
-//							taskEXIT_CRITICAL();
-
-                            //TODO
+                            //^- TODO
 							/////////////////////////////////////////////////////////////
-
-                            /* Don't drive */
-                            retval = FUNC_INCOMPLETE_LIGHT;
-                            break;
                         }
                         else
                         {
-							if(driveGoto(x, y, angle, calc_speed, direction, GOTO_NO_ROUTE, game_state))
-							{
-								retval = FUNC_SUCCESS;
-							}
-							else
+							if(!driveGoto(x, y, angle, calc_speed, direction, GOTO_NO_ROUTE, game_state))
 							{
 								retval = FUNC_ERROR;
 								break;
@@ -392,11 +390,7 @@ func_report_t checkDrive(uint16_t x, uint16_t y, uint16_t angle, uint8_t speed, 
                         is_in_range = 0;
 
                         /* not in range anymore */
-                        if(driveGoto(x, y, angle, speed, direction, GOTO_NO_ROUTE, game_state))
-                        {
-                            retval = FUNC_SUCCESS;
-                        }
-                        else
+                        if(!driveGoto(x, y, angle, speed, direction, GOTO_NO_ROUTE, game_state))
                         {
                             retval = FUNC_ERROR;
                             break;
